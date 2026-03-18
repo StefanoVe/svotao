1f74842095886bf40b4b602bb5e755734790f5ff
StefanoVe <stefano.vecchietti.99@gmail.com>
2026-03-18 13:27:15 +0100

    favicon, rename

--- apps/svotao-p2p-share-client/src/app/app.routes.ts
+++ apps/svotao-p2p-share-client/src/app/app.routes.ts
@@ -9,7 +9,7 @@ export const appRoutes: Route[] = [
   {
     path: 's/rooms/:room',
     loadComponent: () => import('./app').then((m) => m.App),
-    title: 'P2P File Sharing',
+    title: 'Vecho Share',
   },
   // {
   //   path: '**',
